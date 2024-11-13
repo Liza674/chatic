55f050254411f3c98b0ccf15590a6425c1aee6b5
BSCE21007 <91457578+BSCE21007@users.noreply.github.com>
2024-11-13 20:23:46 +0500

    deleted files

--- src/components/typingIndicator.js
+++
@@ -1,8 +0,0 @@
-// src/components/TypingIndicator.js
-import React from 'react';
-
-const TypingIndicator = ({ isTyping }) => {
-    return <div>{isTyping && <p>Someone is typing...</p>}</div>;
-};
-
-export default TypingIndicator;
